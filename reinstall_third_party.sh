688976cf1fe6a9ca42378f990b2c2d4b0b70193b
Tumi Wang <lianyuguhun@gmail.com>
2024-12-06 10:51:11 +0800

    添加第三方库编译框架

---
+++ reinstall_third_party.sh
@@ -0,0 +1,55 @@
+cd $(dirname "$0")
+
+ROOT_DIR=`pwd`
+CURRENT_SYSTEM_NAME=`uname -s`
+CURRENT_SYSTEM_ARCH=`uname -m`
+
+TEMP_DIR=temp
+PREFIX_DIR=prefix/$CURRENT_SYSTEM_NAME-$CURRENT_SYSTEM_ARCH
+THIRD_PARTY_DIR=third_party
+
+if [ -d "$TEMP_DIR" ]; then
+    rm -rf "$TEMP_DIR"
+fi
+mkdir -p "$TEMP_DIR"
+
+if [ -d "$PREFIX_DIR" ]; then
+    rm -rf "$PREFIX_DIR"
+fi
+mkdir -p "$PREFIX_DIR"
+
+# 安装 OpenSSL
+if [ -f "$THIRD_PARTY_DIR/openssl-3.4.0.tar.gz" ]; then
+    build_types=("debug" "release")
+    for BUILD_TYPE in "${build_types[@]}"; do
+        cd "$ROOT_DIR/$TEMP_DIR"
+        tar -xzf "$ROOT_DIR/$THIRD_PARTY_DIR/openssl-3.4.0.tar.gz"
+        cd openssl-3.4.0
+        ./configure --prefix="$ROOT_DIR/$PREFIX_DIR/$BUILD_TYPE" --$BUILD_TYPE no-shared no-docs
+        make
+        # make test
+        make install
+        cd ..
+        rm -rf openssl-3.4.0
+    done
+    cd "$ROOT_DIR"
+fi
+
+# 安装 Boost
+if [ -f "$THIRD_PARTY_DIR/boost-1.86.0-b2-nodocs.tar.gz" ]; then
+    build_types=("debug" "release")
+    for BUILD_TYPE in "${build_types[@]}"; do
+        cd "$ROOT_DIR/$TEMP_DIR"
+        tar -xzf "$ROOT_DIR/$THIRD_PARTY_DIR/boost-1.86.0-b2-nodocs.tar.gz"
+        cd boost-1.86.0
+        ./bootstrap.sh --with-libraries=all
+        ./b2 --prefix="$ROOT_DIR/$PREFIX_DIR/$BUILD_TYPE"  variant=$BUILD_TYPE link=static install
+        cd ..
+        rm -rf boost-1.86.0
+    done
+    cd "$ROOT_DIR"
+fi
+
+if [ -d "$TEMP_DIR" ]; then
+    rm -rf "$TEMP_DIR"
+fi
